f66f8f40a72c958f598ce14125a1a3275ce5d8d7
Shubham <shubham@itsbhm.group>
2021-03-02 10:09:47 +0530

    Added Section 2

---
+++ Section_2/js/index-1.js
@@ -0,0 +1,42 @@
+// JavaScript Variables
+
+// value = undefined by default
+var myVar; // undefined
+
+document.write(myVar);
+
+document.write("<hr>"); // HTML Horizontal Line Break
+
+var myName; // variable declaration
+
+// = Assignment operator
+myName = "John"; // variable initialization 
+
+document.write(myName);
+
+document.write("<hr>"); // HTML Horizontal Line Break
+
+/**
+ * 
+ * Boolean
+ * Double
+ * Float
+ * Int
+ * String
+ * 
+ */
+
+var name = "Mini"; // declaration and initialization at the same time
+
+document.write(name);
+
+document.write("<hr>"); // HTML Horizontal Line Break
+
+myPhone = "iPhone";// declaration and initialization without the var keyword
+
+var x, y, z;
+var x = 10, y = 20, z = 30;
+
+var srNo = 989812;
+
+document.write(srNo);
---
+++ Section_2/js/index-5.js
@@ -0,0 +1,24 @@
+/*     Concatenation "+" Operator     */
+
+// concatenate two strings
+
+var firstName = "Hey";
+var lastName = "Zozo";
+
+var fullName = firstName + " " + lastName;
+
+document.write(fullName);
+
+
+document.write("<br>");
+
+
+// add number & string
+
+var x = 5;
+//var t = "5";
+var t = "Hello";
+var z = t + x; 
+
+document.write(z);
+document.write("<br>");
